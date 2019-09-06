c09bb71087a6c5779263f2a98a38d4576dd58eb2
druzhinin <druzhinin@simicon.com>
2019-09-06 18:52:53 +0300

    Как-то работает

--- albumentations_gui/albumentations_gui.py
+++ albumentations_gui/albumentations_gui.py
@@ -2,10 +2,119 @@ from PyQt5.Qt import *
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 
+from mainwindow_ui import Ui_MainWindow
 
-class AlbumentationsGui(QMainWindow):
+import cv2 as cv
+from PIL import Image, ImageQt
+
+from transforms import TransformsModel
+
+import albumentations as albu
+
+
+class AlbumentationsGui(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(QMainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+
+        self.image = None
+        self.tmp_image = None
+
+        self.transforms_model = TransformsModel(self)
+        self.transforms_tree.setModel(self.transforms_model)
+
+        self.connect()
+
+    def connect(self):
+        self.button_open_image.clicked.connect(self.open_image)
+        self.button_clear.clicked.connect(self.clear_augs)
+        self.button_update_image.clicked.connect(self.update_image)
+
+        self.transforms_tree.doubleClicked.connect(self.add_transform)
+        self.list_widget_selected_transforms.doubleClicked.connect(self.remove_transform)
+
+        # self.transforms_tree.selectionChanged.connect(self.all_docs)
+        # self.list_widget_selected_transforms.selectionChanged.connect(self.added_docs)
+        # self.list_widget_selected_transforms.selectionChanged.connect(self.set_params)
+
+    def open_image(self):
+        path, _ = QFileDialog.getOpenFileName(parent=self,
+                                              caption='Open Image',
+                                              filter='BMP (*.bmp *.dib);;'
+                                                     'JPEG (*.jpeg *.jpg *.jpe);;'
+                                                     'JPEG 2000 (*.jp2);;'
+                                                     'PNG (*.png);;'
+                                                     'WEBP (*.webp);;'
+                                                     'Portable (*.pbm *.pgm *.ppm *.pxm *.pnm);;'
+                                                     'PFM (*.pfm);;'
+                                                     'Sun (*.sr *.ras);;'
+                                                     'TIFF (*.tiff *.tif);;'
+                                                     'OpenEXR (*.exr);;'
+                                                     'HDR (*.hdr *.pic)')
+        if not path:
+            return
+
+        self.image = cv.imread(path, cv.IMREAD_COLOR)
+
+        if self.image is not None:
+            self.image = cv.cvtColor(self.image, cv.COLOR_BGR2RGB)
+            self.line_edit_image_path.setText(path)
+        else:
+            self.line_edit_image_path.setText('')
+
+        self.show_image(self.image)
+
+    def show_image(self, image):
+        if image is None:
+            self.label_image.setText('Empty image')
+            return
+
+        try:
+            self.tmp_image = ImageQt.ImageQt(Image.fromarray(image))
+            self.label_image.setPixmap(QPixmap.fromImage(self.tmp_image))
+        except Exception as err:
+            self.label_image.setText(str(err))
+
+    def add_transform(self, index: QModelIndex):
+        transform = self.transforms_model.transform_name_by_index(index)
+        if not transform:
+            return
+
+        self.list_widget_selected_transforms.addItem(QListWidgetItem(transform))
+
+        self.update_image()
+
+    def remove_transform(self, index: QModelIndex):
+        self.list_widget_selected_transforms.takeItem(index.row())
+
+        self.update_image()
+
+    def all_docs(self, *_, **__):
+        pass
+
+    def added_docs(self, *_, **__):
+        pass
+
+    def set_params(self, *_, **__):
+        pass
+
+    def update_image(self):
+        if self.image is None:
+            return
+
+        transforms = [self.list_widget_selected_transforms.item(i) for i in range(self.list_widget_selected_transforms.count())]
+        transforms = [self.transforms_model.transform_by_name(i.text())() for i in transforms]
+        transform = albu.Compose(transforms)
+
+        try:
+            image = transform(image=self.image)['image']
+            self.show_image(image)
+        except Exception as err:
+            self.label_image.setText(str(err))
+
+    def clear_augs(self):
+        self.list_widget_selected_transforms.clear()
+        self.update_image()
 
 
 if __name__ == '__main__':
---
+++ albumentations_gui/transforms.py
@@ -0,0 +1,131 @@
+from PyQt5.QtCore import *
+
+import inspect
+import albumentations as albu
+
+
+class TransformsModel(QAbstractItemModel):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.transforms = self.get_transforms()
+        (self.pixel_transforms,
+         self.spatial_transforms) = self.split_pixel_spatial_transforms(self.transforms)
+
+        self.name_to_transform = {transform.__name__: transform for transform in self.transforms}
+
+        self.pixel_spatial = {'Pixel transforms': self.pixel_transforms,
+                              'Spatial transforms': self.spatial_transforms}
+
+    @staticmethod
+    def split_pixel_spatial_transforms(transforms):
+        pixel_transforms = []
+        spatial_transforms = []
+
+        for transform in transforms:
+            if issubclass(transform, (albu.ImageOnlyTransform, albu.ImageOnlyIAATransform)):
+                pixel_transforms.append(transform.__name__)
+            elif issubclass(transform, (albu.DualTransform, albu.DualIAATransform)):
+                spatial_transforms.append(transform.__name__)
+
+        return pixel_transforms, spatial_transforms
+
+    @staticmethod
+    def get_transforms():
+        not_include = [
+            albu.BasicTransform,
+            albu.BasicIAATransform,
+            albu.ImageOnlyTransform,
+            albu.DualTransform,
+            albu.ImageOnlyIAATransform,
+            albu.DualIAATransform,
+            albu.Compose,
+        ]
+
+        transforms = []
+        for name in dir(albu):
+            item = getattr(albu, name)
+
+            if not inspect.isclass(item):
+                continue
+
+            if issubclass(item, (albu.BasicTransform, albu.BasicIAATransform)):
+                if item not in not_include:
+                    transforms.append(item)
+
+        return transforms
+
+    def index(self, row, column, parent: QModelIndex=None, *args, **kwargs):
+        if not self.hasIndex(row, column, parent):
+            return QModelIndex()
+
+        if not parent.isValid():
+            return self.createIndex(row, column, list(self.pixel_spatial.keys())[row])
+        else:
+            parent = parent.internalPointer()
+
+        return self.createIndex(row, column,
+                                self.pixel_spatial[parent][row])
+
+    def parent(self, index: QModelIndex=None):
+        if not index.isValid():
+            return QModelIndex()
+
+        child = index.internalPointer()
+
+        if child in self.pixel_spatial:
+            return QModelIndex()
+
+        if child in self.pixel_transforms:
+            return self.createIndex(index.row(), 0, 'Pixel transforms')
+
+        return self.createIndex(index.row(), 0, 'Spatial transforms')
+
+    def rowCount(self, index: QModelIndex=None, *args, **kwargs):
+        value = index.internalPointer()
+
+        if value is None:
+            return len(self.pixel_spatial)
+
+        if value not in self.pixel_spatial:
+            return 0
+
+        count = len(self.pixel_spatial[value])
+        return count
+
+    def columnCount(self, parent=None, *args, **kwargs):
+        return 1
+
+    def data(self, index: QModelIndex, role=None):
+        if not index.isValid() or role != Qt.DisplayRole:
+            return QVariant()
+
+        parent = index.parent().internalPointer()
+        if parent is None:
+            return index.internalPointer()
+
+        data = self.pixel_spatial[parent][index.row()]
+        return data
+
+    def flags(self, index: QModelIndex):
+        if not index.isValid():
+            return Qt.NoItemFlags
+
+        return super().flags(index)
+
+    def headerData(self, section, orientation, role=None):
+        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
+            return 'Transform'
+
+        return QVariant()
+
+    def transform_name_by_index(self, index):
+        data = self.data(index, Qt.DisplayRole)
+
+        if data in self.pixel_spatial:
+            data = None
+
+        return None if isinstance(data, QVariant) else data
+
+    def transform_by_name(self, name):
+        return self.name_to_transform[name]
