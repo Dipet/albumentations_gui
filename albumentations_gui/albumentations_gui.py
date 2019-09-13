a074a476c68dc7df9ea99449e630b8f8ce22cdf5
druzhinin <druzhinin@simicon.com>
2019-09-13 16:19:10 +0300

    Work with code

--- albumentations_gui/albumentations_gui.py
+++ albumentations_gui/albumentations_gui.py
@@ -9,7 +9,10 @@ from PIL import Image, ImageQt
 
 from transforms import TransformsModel
 
-import albumentations as albu
+import albumentations
+
+import numpy as np
+
 
 IMG_EXTENSIONS = {
     'BMP': '*.bmp *.dib',
@@ -33,22 +36,26 @@ class AlbumentationsGui(QMainWindow, Ui_MainWindow):
 
         self.image = None
         self.tmp_image = None
+        self.code_header = 'albumentations.Compose(transforms=['
+        self.code_tail = '])'
 
         self.transforms_model = TransformsModel(self)
         self.transforms_tree.setModel(self.transforms_model)
 
         self.connect()
 
+        self.update_code()
+
     def connect(self):
         self.button_open_image.clicked.connect(self.open_image)
         self.button_clear.clicked.connect(self.clear_augs)
-        self.button_update_image.clicked.connect(self.update_image)
+        self.button_update_image.clicked.connect(self.update_code)
 
         self.transforms_tree.doubleClicked.connect(self.add_transform)
         self.list_widget_selected_transforms.doubleClicked.connect(self.remove_transform)
 
         self.transforms_tree.clicked.connect(self.tree_clicked)
-        self.list_widget_selected_transforms.clicked.connect(self.list_addeg_clicked)
+        self.list_widget_selected_transforms.clicked.connect(self.list_added_clicked)
 
     def open_image(self):
         all_extension = ' '.join([item for key, item in IMG_EXTENSIONS.items()])
@@ -70,7 +77,7 @@ class AlbumentationsGui(QMainWindow, Ui_MainWindow):
         else:
             self.line_edit_image_path.setText('')
 
-        self.show_image(self.image)
+        self.update_image()
 
     def show_image(self, image):
         if image is None:
@@ -85,17 +92,73 @@ class AlbumentationsGui(QMainWindow, Ui_MainWindow):
 
     def add_transform(self, index: QModelIndex):
         transform = self.transforms_model.transform_name_by_index(index)
-        if not transform:
-            return
 
-        self.list_widget_selected_transforms.addItem(QListWidgetItem(transform))
+        self.update_code(add=transform)
+
+    def _get_transforms_from_code(self):
+        code = self.text_edit_code.toPlainText()
+
+        try:
+            transform: albumentations.Compose = eval(code)
+            return transform.transforms.transforms
+        except Exception:
+            return []
+
+    def _get_transform_str(self,
+                           code_transform: albumentations.BasicTransform):
+        transform = code_transform.__class__()
+
+        params1 = {
+            **transform.get_transform_init_args(),
+            **transform.get_base_init_args()
+        }
+        params2 = {
+            **code_transform.get_base_init_args(),
+            **code_transform.get_base_init_args()
+        }
+        diff_params = {}
+
+        for key, value in params2.items():
+            if np.all(value != params1[key]):
+                diff_params[key] = value
+
+        str_transform = ' ' * 4 + 'albumentations.{}({}),\n'
+        str_params = ''
+        for key, value in diff_params.items():
+            str_params += f'{key}={value}, '
+
+        return str_transform.format(str(transform.__class__.__name__), str_params[:-2])
+
+    def update_list(self, transforms):
+        transforms = [i.__class__.__name__ for i in transforms]
+
+        self.list_widget_selected_transforms.clear()
+        self.list_widget_selected_transforms.insertItems(0, transforms)
+
+    def update_code(self, add='', remove=None):
+        transforms = self._get_transforms_from_code()
+
+        if remove is not None:
+            if remove == 'all':
+                transforms = []
+            else:
+                transforms = transforms[:remove] + transforms[remove + 1:]
+
+        if add:
+            transforms.append(self.transforms_model.transform_by_name(add)())
+
+        code = ''.join([self._get_transform_str(i) for i in transforms])
+
+        if code and not code.startswith('\n'):
+            code = '\n' + code
+        self.text_edit_code.setText(f'{self.code_header}{code}{self.code_tail}')
 
         self.update_image()
+        self.update_list(transforms)
 
     def remove_transform(self, index: QModelIndex):
         self.list_widget_selected_transforms.takeItem(index.row())
-
-        self.update_image()
+        self.update_code(remove=index.row())
 
     def tree_clicked(self, index: QModelIndex):
         doc = self.transforms_model.get_docstring(index)
@@ -106,7 +169,7 @@ class AlbumentationsGui(QMainWindow, Ui_MainWindow):
 
         self.text_edit_docstring.setText(doc)
 
-    def list_addeg_clicked(self, index: QModelIndex):
+    def list_added_clicked(self, index: QModelIndex):
         name = self.list_widget_selected_transforms.item(index.row()).text()
 
         doc = self.transforms_model.get_docstring(name)
@@ -121,19 +184,16 @@ class AlbumentationsGui(QMainWindow, Ui_MainWindow):
         if self.image is None:
             return
 
+        code = self.text_edit_code.toPlainText()
         try:
-            transforms = [self.list_widget_selected_transforms.item(i) for i in range(self.list_widget_selected_transforms.count())]
-            transforms = [self.transforms_model.transform_by_name(i.text())() for i in transforms]
-            transform = albu.Compose(transforms)
-
+            transform = eval(code)
             image = transform(image=self.image)['image']
             self.show_image(image)
         except Exception as err:
             self.label_image.setText(str(err))
 
     def clear_augs(self):
-        self.list_widget_selected_transforms.clear()
-        self.update_image()
+        self.update_code(remove='all')
 
 
 if __name__ == '__main__':
